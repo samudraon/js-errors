2a73902cb4018c64d0174898810bf44d663aba04
samudraon <samudraon@gmailcom>
2023-03-06 11:44:14 +0600

    type error added

---
+++ type-error.js
@@ -0,0 +1,12 @@
+// const num = 5;
+// num.push(56);
+
+// let address={};
+// console.log(address.home);
+
+let address = { city: 'bBaria', postCode: 4560 };
+// console.log(address.home.building);
+console.log(address?.home?.building);
+
+const a = 'abul';
+a = 'babul';
