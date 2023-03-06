545488ffa7ff51716297111aa43cd97afc99dcb4
samudraon <samudraon@gmailcom>
2023-03-06 11:26:43 +0600

    syntanx error added

---
+++ syntax-error.js
@@ -0,0 +1,30 @@
+function isBigger(num) {
+    if (num > 5) {
+        return true
+    }
+    else return false
+}
+// }
+
+const result = isBigger(11);
+console.log(result);
+
+const musk = {
+    name: 'Elon',
+    // age:54
+    age: 54,
+    job: 'tesla'
+};
+
+console.log(musk.age);
+
+
+const numbers = [3, 65, 21, 762, 42];
+
+// if (numbers.length > 5) (&& numbers[0] === 5){
+
+// }
+
+if (numbers.length > 5 && numbers[0] === 5){
+
+}
