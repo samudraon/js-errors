a3243a0435709d008d57a6549a95c626833a1eb7
samudraon <samudraon@gmailcom>
2023-03-06 11:51:12 +0600

    reference error added

---
+++ reference-error.js
@@ -0,0 +1,16 @@
+const x = 'Pori';
+const y = 'Moni';
+// console.log(z);
+
+// console.log(name);
+// const name = 'Ananta Jalil';
+// let name = 'Anantan Jalil';
+// var name = 'Anantan Jalil';
+
+console.log(add2(3, 2));
+function add(n1, n2) {
+    return n1+n2
+}
+
+const add2 = (n1, n2) => n1 + n2;
+
